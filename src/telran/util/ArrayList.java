808ceab1ec78835430db78d30cfad03e23ad0259
Pavel Feldman <feldmanpk@gmail.com>
2024-05-20 20:26:16 +0300

    CW #22 Interfaces initial

---
+++ src/telran/util/ArrayList.java
@@ -0,0 +1,67 @@
+package telran.util;
+
+import java.util.Iterator;
+
+public class ArrayList<T> implements List<T> {
+
+	@Override
+	public boolean add(T obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(T pattern) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(T pattern) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(int index, T obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public T remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(T pattern) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int lastIndexOf(T pattern) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
