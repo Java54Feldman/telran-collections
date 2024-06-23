1d0f346040cd6e5ee010c75bff62a324fca13254
Pavel Feldman <feldmanpk@gmail.com>
2024-06-23 19:42:14 +0300

    TreeMap added casting (TreeSet<Entry<K, V>>)set

--- src/telran/util/TreeMap.java
+++ src/telran/util/TreeMap.java
@@ -4,48 +4,43 @@ import java.util.Comparator;
 
 public class TreeMap<K, V> extends AbstractMap<K, V> implements SortedMap<K, V> {
 
+	TreeSet<Entry<K, V>> treeSet = ((TreeSet<Entry<K, V>>)set);
 	@Override
 	public K firstKey() {
-		return ((TreeSet<Entry<K, V>>) set).first().getKey();
+		Entry<K, V> entry = treeSet.first();
+		return entry.getKey();
 	}
 
 	@Override
 	public K lastKey() {
-		return ((TreeSet<Entry<K, V>>) set).last().getKey();
+		Entry<K, V> entry = treeSet.last();
+		return entry.getKey();
 	}
 
 	@Override
 	public K floorKey(K key) {
-		Entry<K, V> pattern = new Entry<>(key, null);
-		Entry<K, V> entry = ((TreeSet<Entry<K, V>>) set).floor(pattern);
-		K res = null;
-		if (entry != null) {
-			res = entry.getKey();
-		}
-		return res;
+		Entry<K, V> entry = treeSet.floor(new Entry<>(key, null));
+		return entry == null ? null : entry.getKey();
 	}
 
 	@Override
 	public K ceilingKey(K key) {
-		Entry<K, V> pattern = new Entry<>(key, null);
-		Entry<K, V> entry = ((TreeSet<Entry<K, V>>) set).ceiling(pattern);
-		K res = null;
-		if (entry != null) {
-			res = entry.getKey();
-		}
-		return res;
+		Entry<K, V> entry = treeSet.ceiling(new Entry<>(key, null));
+		return entry == null ? null : entry.getKey();
 	}
 
 	@Override
 	protected Set<K> getEmptyKeySet() {
+		
 		return new TreeSet<K>();
 	}
 	public TreeMap() {
 		this(Comparator.naturalOrder());
+		
 	}
 	public TreeMap(Comparator<Entry<K, V>> comp) {
 		set = new TreeSet<>(comp);
-		
+		treeSet = ((TreeSet<Entry<K, V>>)set);
 	}
 
 }
